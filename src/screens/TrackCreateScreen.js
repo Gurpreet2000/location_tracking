8be2b1b7b32bf362a515c5c1534a2e89b14e030f
Gurpreet Singh <singhgurpreet1212@gmail.com>
2021-10-05 21:55:27 +0530

    get permission from user to get their location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {Text} from 'react-native-elements';
+import {requestForegroundPermissionsAsync} from 'expo-location';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    let {status} = await requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      setErr('Permission to access location was denied');
+      return;
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <>
       <Text h2>Create a Map</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </>
   );
 };
