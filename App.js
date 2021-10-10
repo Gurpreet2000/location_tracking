d64cbe44b0039842a65e63651b617bcf8ef6df3d
Gurpreet Singh <singhgurpreet1212@gmail.com>
2021-10-10 17:05:05 +0530

    Add sone final styling

--- App.js
+++ App.js
@@ -15,14 +15,28 @@ import {
 } from './src/context/AuthContext';
 import {Provider as LocationProvider} from './src/context/LocationContext';
 import {Provider as TrackProvider} from './src/context/TrackContext';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Stack = createStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
 const trackListFlow = () => (
-  <Stack.Navigator screenOptions={{headerShown: false}}>
-    <Stack.Screen name="TrackList" component={TrackListScreen} />
-    <Stack.Screen name="TrackDetail" component={TrackDetailScreen} />
+  <Stack.Navigator screenOptions={{}} initialRouteName="TrackList">
+    <Stack.Screen
+      name="TrackList"
+      component={TrackListScreen}
+      options={{
+        title: 'Tracks',
+        headerStyle: {backgroundColor: '#e91e63'},
+        headerTintColor: '#eee',
+        headerTitleStyle: {fontWeight: 'bold'},
+      }}
+    />
+    <Stack.Screen
+      name="TrackDetail"
+      component={TrackDetailScreen}
+      options={{title: ''}}
+    />
   </Stack.Navigator>
 );
 
@@ -35,13 +49,42 @@ const App = () => {
   return (
     <NavigationContainer>
       {isSignedIn ? (
-        <Tab.Navigator>
-          <Tab.Screen name="Tracks" component={trackListFlow} />
-          <Tab.Screen name="TrackCreate" component={TrackCreateScreen} />
-          <Tab.Screen name="TrackAccount" component={AccountScreen} />
+        <Tab.Navigator activeColor="#e91e63">
+          <Tab.Screen
+            name="Tracks"
+            component={trackListFlow}
+            options={{
+              title: 'Add Track',
+              tabBarIcon: ({color}) => (
+                <Icon name="th-list" color={color} size={20} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="TrackCreate"
+            component={TrackCreateScreen}
+            options={{
+              title: 'Tracks',
+              tabBarIcon: ({color}) => (
+                <Icon name="plus" color={color} size={20} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="TrackAccount"
+            component={AccountScreen}
+            options={{
+              title: 'Account',
+              tabBarIcon: ({color}) => (
+                <Icon name="gear" color={color} size={20} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       ) : (
-        <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}
+          initialRouteName="Signin">
           <Stack.Screen name="Signup" component={SignupScreen} />
           <Stack.Screen name="Signin" component={SigninScreen} />
         </Stack.Navigator>
